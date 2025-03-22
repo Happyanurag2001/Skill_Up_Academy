2fcecb7de2f7bfe55d3ef13a8ddd161c4d116e3f
Anurag Singh Kushwaha <117572257+Happyanurag2001@users.noreply.github.com>
2025-03-22 16:12:56 +0530

    Delete Skill_Up_Academy directory

--- Skill_Up_Academy/js/loader.js
+++
@@ -1,8 +0,0 @@
-var loader = document.getElementById("preloader");
-
-window.addEventListener("load", function() {
-  // Set a delay (e.g., 2 seconds) before hiding the loader
-  setTimeout(function() {
-    loader.style.display = "none";
-  }, 1500); // 2000 milliseconds = 2 seconds
-});
